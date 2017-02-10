47036ce2895145c7a4113a84270bf6df71235c5c
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2017-02-10 20:16:24 +0100

    Reformat code

--- app/Transformers/ScammerTransformer.php
+++ app/Transformers/ScammerTransformer.php
@@ -12,9 +12,9 @@ class ScammerTransformer extends Transformer
     public function transform($scammer) : array
     {
         return [
-            'id'          => (int) $scammer->id,
-            'first_name'  => $scammer->first_name,
-            'last_name'   => $scammer->last_name,
+            'id'         => (int) $scammer->id,
+            'first_name' => $scammer->first_name,
+            'last_name'  => $scammer->last_name,
         ];
     }
 }
