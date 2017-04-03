304460165d43c4e669eee7ab343b8a84e280d3cf
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2017-04-03 13:21:39 +0200

    Update dependencies

--- resources/frontend/src/bootstrap.js
+++ resources/frontend/src/bootstrap.js
@@ -64,6 +64,9 @@ Object.defineProperty(Vue.prototype, '$http', {
  * https://github.com/laravel/echo
  */
 import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
 
 const echo = new Echo({
   broadcaster: 'pusher',
