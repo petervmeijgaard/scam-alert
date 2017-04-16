11de22872cde31c2d5d3d5739ea49427cd4191cb
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2017-04-16 23:46:11 +0200

    Refactor the <v-drawer>-component

--- resources/frontend/src/app/layouts/base/base.js
+++ resources/frontend/src/app/layouts/base/base.js
@@ -1,6 +1,11 @@
 import { mapState } from 'vuex';
 import * as VContent from './../../components/content/content.vue';
 import * as VDrawer from './../../components/drawer/drawer.vue';
+import * as VDrawerHeader from './../../components/drawer/header/header.vue';
+import * as VDrawerTitle from './../../components/drawer/title/title.vue';
+import * as VDrawerBody from './../../components/drawer/body/body.vue';
+import * as VDrawerFooter from './../../components/drawer/footer/footer.vue';
+import * as VNavIcon from './../../components/nav-icon/nav-icon.vue';
 import * as VHeader from './../../components/header/header.vue';
 import * as VOverlay from './../../components/overlay/overlay.vue';
 import * as VToolbar from './../../components/toolbar/toolbar.vue';
@@ -14,6 +19,9 @@ export default {
     return {
       title: {
         content: 'Scam Alert',
+      },
+      navIcon: {
+        variant: 'green',
       },
       routes: [
         {
@@ -42,6 +50,11 @@ export default {
   components: {
     VContent,
     VDrawer,
+    VDrawerHeader,
+    VNavIcon,
+    VDrawerTitle,
+    VDrawerFooter,
+    VDrawerBody,
     VHeader,
     VOverlay,
     VToolbar,
